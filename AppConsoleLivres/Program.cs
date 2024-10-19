d7db050610da698c94d06224073c25e96af900b9
Ahmed <300130276@monboreal.ca>
2024-10-18 21:59:21 -0400

    code complet pour la classe Program

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppConsoleLivres
 {
-    private static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Revue revue1 = new Revue("Le Mans 24 Heures", "Michel Vaillant", 120, 45, 2022);
+            Revue revue2 = new Revue("Légendes du Nürburgring", "Hans Müller", 80, 12, 2021);
+            Revue revue3 = new Revue("Le Mans Classique", "Jacques Durand", 100, 67, 2023);
+
+            Roman roman1 = new Roman("La course des géants", "Jean-Pierre Gauthier", 350, "Sport Automobile");
+            Roman roman2 = new Roman("Les Rois du Nürburgring", "Klaus Schmidt", 400, "Histoire du Nürburgring");
+            Roman roman3 = new Roman("Le Mans : Une légende", "Antoine Dupuis", 450, "Chronique de Le Mans");
+
+            revue1.AfficherDetails();
+            revue2.AfficherDetails();
+            revue3.AfficherDetails();
+
+            roman1.AfficherDetails();
+            roman2.AfficherDetails();
+            roman3.AfficherDetails();
+
+            List<Livre> listeLivres = new List<Livre> { revue1, revue2, revue3, roman1, roman2, roman3 };
+
+            Console.WriteLine("\nAffichage de la liste des livres:");
+            foreach (var livre in listeLivres)
+            {
+                livre.AfficherDetails();
+            }
+        }
     }
 }
--- AppConsoleLivres/Roman.cs
+++ AppConsoleLivres/Roman.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppConsoleLivres
+﻿namespace AppConsoleLivres
 {
-    internal class Roman
+    public class Roman : Livre
     {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, int nbrPages, string genre)
+            : base(titre, auteur, nbrPages)
+        {
+            Genre = genre;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NbrPages}, Genre: {Genre}");
+        }
     }
+
 }
