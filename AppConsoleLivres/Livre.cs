f4f077d56ef698010943188c728f4d93476f07b3
Ahmed <300130276@monboreal.ca>
2024-10-18 21:49:36 -0400

    code complet pour la classe Livre

--- AppConsoleLivres/Livre.cs
+++ AppConsoleLivres/Livre.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppConsoleLivres
 {
-    internal class Livre
+    public class Livre
     {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NbrPages { get; set; }
+
+        public Livre(string titre, string auteur, int nbrPages)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NbrPages = nbrPages;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NbrPages}");
+        }
     }
 }
